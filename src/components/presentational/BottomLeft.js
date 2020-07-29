4b623a8260d4bd991e7280a5bd9500b4c6499dc8
forison <addoboakyeforison@gmail.com>
2020-07-29 07:02:10 -0400

    add project organization and linters

---
+++ src/components/presentational/BottomLeft.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BottomLeft extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default BottomLeft;
